c3ef0a7a3efcf75e009437089201173a432dbae1
MHIRI Mohamed <mmhiri@amaris.com>
2019-03-08 19:30:30 +0100

    simple log4net in progress

--- LoggingExample/Controllers/ValuesController.cs
+++ LoggingExample/Controllers/ValuesController.cs
@@ -4,14 +4,19 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using log4net;
 
 namespace LoggingExample.Controllers
 {
     public class ValuesController : ApiController
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         // GET api/values
         public IEnumerable<string> Get()
         {
+            log.Info("Hello logging world!");
+            Console.WriteLine("Hit enter");
             return new string[] { "value1", "value2" };
         }
 
